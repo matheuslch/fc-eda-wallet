2bc917e12246c3f60df8ec14c9f199b7f3751ece
matheuslch <156261531+matheuslch@users.noreply.github.com>
2024-03-18 19:14:30 -0300

    Update entrypoint.sh

--- ms-wallet-core/entrypoint.sh
+++ ms-wallet-core/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-migrate -path /app/internal/database/migrations -database "mysql://root:root@tcp(wallet-core-mysql:3307)/wallet?charset=utf8&parseTime=True&loc=Local" up & tail -f /dev/null
+migrate -path /app/internal/database/migrations -database "mysql://root:root@tcp(wallet-core-mysql:3307)/wallet?charset=utf8&parseTime=True&loc=Local" up \
+& go mod tidy \
+& go run /app/cmd/walletcore/main.go \
+& tail -f /dev/null
