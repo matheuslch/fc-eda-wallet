b76c5355713879455fec288ecec09555dad393bd
matheuslch <156261531+matheuslch@users.noreply.github.com>
2024-03-18 20:16:40 -0300

    Update entrypoint.sh

--- ms-wallet-balance/entrypoint.sh
+++ ms-wallet-balance/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 migrate -path /app/internal/database/migrations -database "mysql://root:root@tcp(wallet-balance-mysql:3308)/wallet?charset=utf8&parseTime=True&loc=Local" up \
-& go mod tidy \
-& go run /app/cmd/walletbalance/main.go \
+& chmod +x /app/cmd/walletbalance/main \
+& ./cmd/walletbalance/main \
 & tail -f /dev/null
